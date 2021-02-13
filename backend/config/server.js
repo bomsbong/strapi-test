746d1bb79924accd5571f61d6eca764bc4810506
bomsbong <bomcor@gmail.com>
2021-02-13 11:48:13 +0800

    commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e9511c50943d25e306be0d1bb20990db'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b2eac393-ecaf-4723-94a3-05d987eb1bd4'
+};
